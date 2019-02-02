f46d4e3d4698aee2b4870a8ed50b0fa282506f25
dagma <dagmara.ziolkowska@wp.pl>
2019-02-02 22:36:04 +0100

    Dostosowanie pod tablice dwuwymiarowe

--- src/ArrayComparator.java
+++ src/ArrayComparator.java
@@ -1,12 +1,17 @@
 public class ArrayComparator {
 
-    public static boolean compare(int[] tab1, int[] tab2) {
+    public static boolean compare(int[][] tab1, int[][] tab2) {
         if (tab1.length != tab2.length)
             return false;
         else {
             for (int i = 0; i < tab1.length; i++) {
-                if (tab1[i] != tab2[i])
+                if (tab1[i].length != tab2[i].length) {
                     return false;
+                }
+                for (int j = 0; j < tab1[i].length; j++) {
+                    if (tab1[i][j] != tab2[i][j])
+                        return false;
+                }
             }
             return true;
         }
--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,7 +1,13 @@
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] array1 = new int[]{0};
-        int[] array2 = new int[] {1,3,5};
+        int[][] array1 = new int[2][1];
+        int[][] array2 = new int[2][2];
+
+        array1[0] = new int[]{1};
+        array1[1] = new int[]{3,4};
+        array2[0] = new int[]{1,2};
+        array2[1] = new int[]{3,4};
+
         System.out.println(ArrayComparator.compare(array1, array2));
     }
 }
